403c2a174445a831a3ed1cdde9545b5223f97351
harvey banning jr <hbanningjr@gmail.com>
2026-05-12 20:34:37 -0400

    Adds contract deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,16 +7,23 @@
 const hre = require("hardhat");
 
 async function main() {
-  const NAME = 'Dapp University'
-  const SYMBOL = 'DAPP'
-  const MAX_SUPPLY = '1000000'
+  const Token = await hre.ethers.getContractFactory("Token");
 
-  // Deploy Token
-  const Token = await hre.ethers.getContractFactory('Token')
-  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
+  // Deploy (token 1)
+  let DAPPU = await Token.deploy("DAPPU Token", "DAPPU", "1000000"); // 1 million tokens
+  await DAPPU.deployed();
+  console.log(`DAPPU token deployed to: ${DAPPU.address}\n`);
 
-  await token.deployed()
-  console.log(`Token deployed to: ${token.address}\n`)
+  // Deploy (token 2)
+  let usd = await Token.deploy("USD Token", "USD", "1000000"); // 1 million tokens
+  await usd.deployed();
+  console.log(`DAPPU token deployed to: ${usd.address}\n`);
+
+  // Deploy Auto Market Maker
+  const AMM = await hre.ethers.getContractFactory("AMM");
+  const amm = await AMM.deploy(DAPPU.address, usd.address);
+
+  console.log(`AMM contract deployed to${amm.address}\n:`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
