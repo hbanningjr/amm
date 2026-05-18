224e7e8c80327487a35effbcbd2f3402a6c9eec8
harvey banning jr <hbanningjr@gmail.com>
2026-05-18 12:17:03 -0400

    Wires up Redux

---
+++ src/store/interactions.js
@@ -0,0 +1,32 @@
+import { ethers } from "ethers";
+
+import {
+  setAccount,
+  setProvider,
+  setNetwork,
+} from "../store/reducers/provider";
+
+export const loadProvider = (dispatch) => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  dispatch(setProvider(provider));
+
+  return provider;
+};
+
+export const loadNetwork = async (provider, dispatch) => {
+  const { chainId } = await provider.getNetwork();
+  dispatch(setNetwork(chainId));
+
+  return chainId;
+};
+
+export const loadAccount = async (dispatch) => {
+  const accounts = await window.ethereum.request({
+    method: "eth_requestAccounts",
+  });
+  const account = ethers.utils.getAddress(accounts[0]);
+  setAccount(account);
+  dispatch(setAccount(account));
+
+  return account;
+};
