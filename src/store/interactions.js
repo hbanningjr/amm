c9fcdb8fbf288db8f7359c1b72f604740ae260de
harvey banning jr <hbanningjr@gmail.com>
2026-05-19 18:05:27 -0400

    Adds swap component

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -4,7 +4,13 @@ import { setAccount, setProvider, setNetwork } from "./reducers/provider";
 
 import { setContracts, setSymbols, balancesLoaded } from "./reducers/tokens";
 
-import { setContract, sharesLoaded } from "./reducers/amm";
+import {
+  setContract,
+  sharesLoaded,
+  swapRequest,
+  swapSuccess,
+  swapFail,
+} from "./reducers/amm";
 
 import TOKEN_ABI from "../abis/Token.json";
 import AMM_ABI from "../abis/AMM.json";
@@ -79,4 +85,31 @@ export const loadBalances = async (amm, tokens, account, dispatch) => {
   );
   const shares = await amm.shares(account);
   dispatch(sharesLoaded(ethers.utils.formatUnits(shares.toString(), "ether")));
+};
+
+// -------------------------------------------------------------
+// SWAP
+
+export const swap = async (provider, amm, token, symbol, amount, dispatch) => {
+  try {
+    dispatch(swapRequest());
+
+    let transaction;
+
+    const signer = await provider.getSigner();
+
+    transaction = await token.connect(signer).approve(amm.address, amount);
+    await transaction.wait();
+
+    if (symbol === "DAPPU") {
+      transaction = await amm.connect(signer).swapToken1(amount);
+    } else {
+      transaction = await amm.connect(signer).swapToken2(amount);
+    }
+
+    await transaction.wait();
+    dispatch(swapSuccess(transaction.hash));
+  } catch (error) {
+    dispatch(swapFail());
+  }
 };
